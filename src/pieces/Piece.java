12aaed262ce1da9856eec28f68d4827d96ebfa12
Shalin <shalinmehta20@gmail.com>
2017-11-11 15:08:34 -0500

    Created pieces package and classes, along with some starter interface methods

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -1,6 +1,37 @@
 package pieces;
+import java.util.List;
 
-/** Represent a piece on the chess board */
+/** Represents a piece on the chess board */
 public interface Piece {
 
+	/** Moves the piece to a new position on the board
+	 * Requires: decision is a valid move
+	 */
+	public void move();
+	
+	/** Sets the position of a piece on the board 
+	 * @param col
+	 * 		The column the piece is now in
+	 * @param row
+	 * 		The row the piece is now in
+	 */
+	public void setPosition(int col, int row);
+	
+	/**
+	 * @return
+	 * 		The row the piece is currently in
+	 */
+	public int getRow();
+	
+	/**
+	 * @return
+	 * 		The column the piece is currently in
+	 */
+	public int getCol();
+	
+	/** 
+	 * @return
+	 * 		A list of valid moves that a given piece can make
+	 */
+	public List getValidMoves();
 }
---
+++ src/pieces/Rook.java
@@ -0,0 +1,37 @@
+package pieces;
+
+import java.util.List;
+
+public class Rook implements Piece {
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPosition(int col, int row) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getRow() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCol() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List getValidMoves() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
